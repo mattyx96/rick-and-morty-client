5ce01a6330df7d5d3dad202f7b270d7f9be66e4c
Matteo Omicini <matteoomicini.dev@gmail.com>
2025-09-18 13:15:36 +0200

    feat: location + episode single pages

---
+++ app/episodes/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { CharactersGrid } from "@/components/characters-grid";
+import { fetchEpisode } from "@/services/rick-and-morty-api-service";
+
+interface EpisodePageProps {
+  params: { id: string };
+}
+
+export default async function EpisodeDetailPage({ params }: EpisodePageProps) {
+  const episode = await fetchEpisode(Number(params.id));
+
+  return (
+    <main className="container mx-auto flex flex-col grow px-4 py-8">
+      <div>
+        <h1 className="text-3xl font-bold mb-4">{episode.name}</h1>
+        <p className="text-lg mb-2">
+          <span className="font-semibold">Episode:</span> {episode.episode}
+        </p>
+        <p className="text-lg mb-6">
+          <span className="font-semibold">Air Date:</span> {episode.air_date}
+        </p>
+      </div>
+
+      <div className="mt-20">
+        <h2 className="text-2xl font-semibold mb-4">Characters</h2>
+        <CharactersGrid ids={episode.characters} />
+      </div>
+    </main>
+  );
+}
---
+++ app/locations/[id]/page.tsx
@@ -0,0 +1,39 @@
+import { CharactersGrid } from "@/components/characters-grid";
+import { fetchLocation } from "@/services/rick-and-morty-api-service";
+
+interface LocationPageProps {
+  params: { id: string };
+}
+
+export default async function LocationDetailPage({
+  params,
+}: LocationPageProps) {
+  const location = await fetchLocation(Number(params.id));
+
+  return (
+    <main className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold mb-4">{location.name}</h1>
+
+      <div className="mb-6 text-lg space-y-1">
+        <p>
+          <span className="font-semibold">Type:</span> {location.type}
+        </p>
+        <p>
+          <span className="font-semibold">Dimension:</span> {location.dimension}
+        </p>
+        <p>
+          <span className="font-semibold">Created:</span>{" "}
+          {new Date(location.created).toLocaleDateString()}
+        </p>
+      </div>
+
+      <h2 className="text-2xl font-semibold mb-4">Residents</h2>
+
+      {location.residents.length === 0 ? (
+        <p className="text-muted-foreground">No known residents.</p>
+      ) : (
+        <CharactersGrid ids={location.residents} />
+      )}
+    </main>
+  );
+}
